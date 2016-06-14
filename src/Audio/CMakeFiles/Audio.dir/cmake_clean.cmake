9377eb7a6f42d269dce5a8ab38d3068ce5aa9296
Tashrif Sanil <tashrifsanil@protonmail.com>
2016-06-15 02:26:07 +0400

    Full array based spectrum painting

---
+++ src/Audio/CMakeFiles/Audio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Audio.dir/playback.cpp.o"
+  "libAudio.pdb"
+  "libAudio.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Audio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Spectrum/CMakeFiles/Spectrum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spectrum.dir/spectrum.cpp.o"
+  "libSpectrum.pdb"
+  "libSpectrum.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spectrum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
