b5f5a02b24988b4f19e42ca37af7f271dda62799
Tashrif Sanil <tashrifsanil@protonmail.com>
2016-07-17 01:32:50 +0400

    Removed need for recursion in directory scanning

--- src/Audio-Library/audio-library.cpp
+++ src/Audio-Library/audio-library.cpp
@@ -22,11 +22,8 @@ AudioLibrary::~AudioLibrary(){}
 sqlite3 *library_db;
 bool db_opened;
 
-GDir *dir=NULL;
-GError *error=NULL;
-
 vector<char*> file_locations;
-vector<char*> subdir_locations;
+std::vector<std::string> subdir_locations;
 vector<char*> omitted_locations;
 
 int file_count;
@@ -39,48 +36,41 @@ void AudioLibrary::scan_dir(const char *dir_location)
   std::cout << "No. of Subdirectories : " << subdir_count << std::endl;
 }
 
-//AudioLibrary::AudioLibrary::scan_dir recursion variables
-int subdir_iter=0;
-int subdir_iter_amount=0;
-bool subdir_iter_init = false;
-//end
-
 void AudioLibrary::scan_dir_backend(const char *dir_location){
-  string file_location = dir_location;
-  if (file_location.substr(file_location.length() - 1) != "/"){
-    file_location += "/";
-  }
-  const char *file;
-  dir = g_dir_open(dir_location, 0, &error);
-  while ((file = g_dir_read_name(dir))){
-    try {
-      GError *subdir_error = NULL;
+  subdir_locations.push_back(dir_location);
 
-      string subdir_location = file_location + file;
-      g_dir_open(util::to_char(subdir_location), 0, &subdir_error);
-      if (subdir_error == NULL) {
-        subdir_count++;
-        subdir_locations.push_back(util::to_char(subdir_location));
+  for (size_t subdir_iter = 0; subdir_iter < subdir_locations.size(); subdir_iter++){
+    std::string file_location = subdir_locations[subdir_iter];
+    if (file_location.substr(file_location.length() - 1) != "/")
+      file_location += "/";
+
+    GError *error = NULL;
+    GError *subdir_error = NULL;
+
+    const char *file;
+    GDir *dir = g_dir_open(subdir_locations[subdir_iter].c_str(), 0, &error);
+
+    while ((file = g_dir_read_name(dir))){
+      try {
+	GError *subdir_error = NULL;
+
+	string subdir_location = file_location + file;
+	g_dir_open(util::to_char(subdir_location), 0, &subdir_error);
+	if (subdir_error == NULL) {
+	  subdir_count++;
+	  subdir_locations.push_back(util::to_char(subdir_location));
+	}
+	if (subdir_error != NULL){
+	  throw 0;
+	}
       }
-      if (subdir_error != NULL){
-        throw 0;
-      }
+      catch (int exception)
+	{
+	  file_count++;
+	  file_location += file;
+	  //std::cout << "File Location : " << file_location << std::endl;
+	  //std::cout << "Subdir : " << subdir_locations[subdir_iter] << std::endl;
+	}
     }
-    catch (int exception)
-    {
-      file_count++;
-      file_location += file;
-    }
-  }
-  if (subdir_iter_init == true)
-  {
-    subdir_iter++;
-  }
-  if (subdir_locations.size() != 0){
-    subdir_iter_amount = subdir_locations.size();
-  }
-  if (subdir_iter < subdir_iter_amount){
-    subdir_iter_init = true;
-    AudioLibrary::scan_dir_backend(subdir_locations[subdir_iter]);
   }
 }
