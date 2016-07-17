00d0c25c9fe32ddd85b0ff66e7c8246f5932b15f
Tashrif Sanil <tashrifsanil@protonmail.com>
2016-07-17 15:01:41 +0400

    Cleaned up directory scanning function

--- src/Audio-Library/audio-library.cpp
+++ src/Audio-Library/audio-library.cpp
@@ -1,20 +1,10 @@
-#include <algorithm>
 #include <gtkmm.h>
 #include <GUI/gui.h>
 #include <iostream>
-#include <dirent.h>
-#include <fstream>
-#include <Utilities/btree.h>
-#include <string>
 #include <sqlite3.h>
 #include <Utilities/util.h>
-#include <taglib/taglib.h>
 #include <Audio-Info/audioinfo.h>
 #include <Audio-Library/audio-library.h>
-#include <ctime>
-#include <future>
-#include <thread>
-using namespace std;
 
 AudioLibrary::AudioLibrary(){}
 AudioLibrary::~AudioLibrary(){}
@@ -22,21 +12,16 @@ AudioLibrary::~AudioLibrary(){}
 sqlite3 *library_db;
 bool db_opened;
 
-vector<char*> file_locations;
+std::vector<char*> file_locations;
 std::vector<std::string> subdir_locations;
-vector<char*> omitted_locations;
+std::vector<char*> omitted_locations;
 
+  
 int file_count;
 int subdir_count;
 
-void AudioLibrary::scan_dir(const char *dir_location)
-{
-  AudioLibrary::scan_dir_backend(dir_location);
-  std::cout << "No. of Files : " << file_count << std::endl;
-  std::cout << "No. of Subdirectories : " << subdir_count << std::endl;
-}
+void AudioLibrary::scan_dir(const char *dir_location){
 
-void AudioLibrary::scan_dir_backend(const char *dir_location){
   subdir_locations.push_back(dir_location);
 
   for (size_t subdir_iter = 0; subdir_iter < subdir_locations.size(); subdir_iter++){
@@ -68,9 +53,9 @@ void AudioLibrary::scan_dir_backend(const char *dir_location){
 	{
 	  file_count++;
 	  file_location += file;
-	  //std::cout << "File Location : " << file_location << std::endl;
-	  //std::cout << "Subdir : " << subdir_locations[subdir_iter] << std::endl;
 	}
     }
   }
+  std::cout << "No. of Files : " << file_count << std::endl;
+  std::cout << "No. of Subdirectories : " << subdir_count << std::endl;
 }
